cc68f330845098d966db196f81647735a73788b5
Alan Silva <alancristiansouza7@gmail.com>
2026-06-12 10:28:18 -0300

    fix: configure OpenAPI server URL for Railway deployment

--- src/main/java/com/alansilva/financas_api/config/OpenApiConfig.java
+++ src/main/java/com/alansilva/financas_api/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.alansilva.financas_api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.expression.spel.ast.OpNE;
@@ -14,6 +15,11 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
 
         return new OpenAPI()
+                .addServersItem(
+                        new Server()
+                                .url("https://financas-api-production-cac9.up.railway.app")
+                                .description("Produção Railway")
+                )
                 .info(new Info()
                         .title("Financas API")
                         .version("1.0")
